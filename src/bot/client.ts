be80c570ad51f41252e4cc0086ea4021646c4091
havuh <alejandr.jacobo@gmail.com>
2025-05-11 17:07:58 -0500

    fix: disable sandbox with puppeteer

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,5 +1,10 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
+import { executablePath } from "puppeteer";
 
 export const client = new Client({
   authStrategy: new LocalAuth({ clientId: "new-client" }),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: executablePath(), 
+  },
 });
