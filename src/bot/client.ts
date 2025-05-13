89f290e574315d3fa0bf498fc782d8ebef6acbf2
havuh <alejandr.jacobo@gmail.com>
2025-05-13 00:19:05 -0500

    fix: chromium

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,10 +1,9 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
-import { executablePath } from "puppeteer";
 
 export const client = new Client({
   authStrategy: new LocalAuth({ clientId: "new-client" }),
   puppeteer: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: executablePath(), 
+    executablePath: '/usr/bin/chromium'
   },
 });
